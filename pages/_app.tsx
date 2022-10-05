c150cb965fd5d35fb7a1ea23cfd6d67f4ad8ca14
Pablo Apausa Chamorro <pablo@MacBook-Pro-de-Pablo.local>
2022-10-05 17:57:13 +0200

    Install Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
 
 export default MyApp;
