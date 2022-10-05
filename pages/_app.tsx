ed636cfa9f35d55ad30a16accc779c10865b95e5
Pablo Apausa Chamorro <pablo@MacBook-Pro-de-Pablo.local>
2022-10-05 18:05:31 +0200

    Install TanStack Query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider>
-    <Component {...pageProps} />
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
   </ChakraProvider>
 );
 
